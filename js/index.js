03d46c7fd03f024e4302f2c4357ffca190f40309
SergioZenz <sergioeezzz@gmail.com>
2025-12-03 22:38:23 -0300

    Se incorporó el carrito en las 3 paginas

--- js/index.js
+++ js/index.js
@@ -1,1 +1,75 @@
+//Creo un div contenedor para los favoritos y le agrego una clase 
+const contenedorFavoritos = document.createElement('div');
+contenedorFavoritos.classList.add('contenedor-favoritos');
 
+let seccionFavs = document.getElementById("seccion_favs");
+
+let tituloCarrito = document.getElementById('titulo_favs');
+
+let totalFavoritos = document.getElementById('item_cantidad');
+
+let precioTotal = document.getElementById('precio_total');
+
+function loadFavourites() {
+  try {
+    contenedorFavoritos.innerHTML = ""; // línea para evitar duplicados
+
+    let totalPrice = 0;
+
+    if(localStorage.length > 0) {
+      seccionFavs.style.display = "block";
+      document.getElementById("carrito-panel").style.display = "none"; // inicio oculto
+      totalFavoritos.textContent = localStorage.length; // actualiza el número en el ícono
+      totalFavoritos.innerText = localStorage.length;
+    }
+    Object.keys(localStorage).forEach(function(key) {
+      let item = JSON.parse(localStorage.getItem(key));
+      totalPrice += item.price;
+      contenedorFavoritos.innerHTML += `
+            <div class="card-fav">
+              <img id="imagen" src="${item.image}"></img>
+              <h5>${item.price} $</h5>
+              <h5>${item.name}</h5>
+              <div>
+                <button onclick="eliminar(${item.favId})" class="btn-del" value="Eliminar">Eliminar ❌</button>
+              </div>
+            </div>
+        `;
+    });
+    precioTotal.innerText = 'Total : '.concat(totalPrice).concat(' $');
+    seccionFavs.appendChild(contenedorFavoritos);
+    } catch (error) {
+    console.error("Error al obtener los favoritos:", error);
+  }
+  if (localStorage.length < 1) {
+    seccionFavs.style.display = "none"; // Linea que oculta el carrito al NO haber producto seleccionados
+  }
+}
+
+document.addEventListener("DOMContentLoaded", loadFavourites);
+
+function eliminar(id) {
+  let idx = id.toString();
+  console.log(idx);
+  localStorage.removeItem(idx);
+  loadFavourites();
+}
+
+//boton deleteAll del html que elimina todos los favoritos.
+const btnDeleteAll = document.getElementById('delete_all');
+btnDeleteAll.addEventListener('click', eliminarDeseados);
+
+async function eliminarDeseados() {
+  try {
+    localStorage.clear();
+    loadFavourites();
+  } catch(error) {
+    console.log(error);
+  }
+}
+
+// Toggle del carrito
+document.getElementById("carrito-icono").addEventListener("click", () => {
+  const panel = document.getElementById("carrito-panel");
+  panel.style.display = (panel.style.display === "flex") ? "none" : "flex";
+});
